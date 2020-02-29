a093e093342ecc503f44c2fc73e3657721cdcd1d
Camposm97 <camposm1205@gmail.com>
2020-02-29 13:16:42 -0500

    Created python project with small demo

---
+++ Demo.py
@@ -0,0 +1,14 @@
+# This is a comment 
+'Another way to make a comment'
+
+firstName = "Michael"
+lastName = "Campos"
+fullName = firstName + " " + lastName
+age = 22
+print("Original: " + fullName)
+print("UPPER: " + fullName.upper())
+print("lower: " + fullName.lower())
+print("Title: " + fullName.title())
+print("Rstrip: " + "A B C".rstrip())
+
+
